7bff517bfc1171ee64a618053af4fab570b4138c
Flame <5017_30@mail.ru>
2024-12-16 09:43:35 +0300

    fix: header

--- src/containers/Layout/index.tsx
+++ src/containers/Layout/index.tsx
@@ -2,11 +2,17 @@ import { type FC } from 'react'
 import { Outlet } from 'react-router-dom'
 import { StyledContent, StyledLayout } from './Layout.styled.tsx'
 import { Header, Sidebar } from '@/components'
+import { useAppSelector } from '@/hooks'
 
 const Layout: FC = () => {
+    const user = useAppSelector(state => state.auth.user)
+
     return (
         <>
-            <Header/>
+            {!!user.role && (
+                <Header/>
+            )}
+
             <StyledLayout hasSider>
                 <Sidebar />
                 <StyledContent>
