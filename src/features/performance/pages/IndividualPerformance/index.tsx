af7be657674a9134086a89c4269b6460e3dedf3c
Flame <5017_30@mail.ru>
2025-03-03 23:14:29 +0300

    fix: IndividualPerformance

--- src/features/performance/pages/IndividualPerformance/index.tsx
+++ src/features/performance/pages/IndividualPerformance/index.tsx
@@ -1,20 +1,25 @@
 import { type FC } from 'react'
 import { useLocation } from 'react-router-dom'
-import { Card } from 'antd'
+import { Card, Tabs } from 'antd'
 import GradesChart from '../../components/GradesChart'
 import { useGetAllGradesFromStudentQuery } from '../../api/performance.api.ts'
 
 const IndividualPerformance: FC = () => {
     const { state } = useLocation()
-
     const { data: gradesData } = useGetAllGradesFromStudentQuery(state.id)
 
     return (
         <Card>
-            {state.id}
-            {gradesData?.map((item, index) => (
-                <GradesChart key={index} data={item} />
-            ))}
+            {gradesData && (
+                <Tabs
+                    items={gradesData
+                        .map((item, index) => ({
+                            key: `${index}`,
+                            label: item.discipline,
+                            children: <GradesChart key={index} data={item} />
+                        }))}
+                />
+            )}
         </Card>
     )
 }
