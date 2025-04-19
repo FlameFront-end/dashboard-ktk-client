b66c861365a1350d426e99fc0ec733c3b89d9ec8
FlameFront-end <5017_30@mail.ru>
2025-04-20 01:48:04 +0300

    fix: style header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,26 +5,42 @@ import { StyledHeaderWrapper } from './Header.styled.tsx'
 import { useAppSelector } from '@/hooks'
 
 const roles = {
-    teacher: 'учитель',
-    admin: 'администатор',
-    student: 'студент'
+	teacher: 'учитель',
+	admin: 'администатор',
+	student: 'студент'
 }
 
 const Header: FC = () => {
-    const user = useAppSelector(state => state.auth.user)
+	const user = useAppSelector(state => state.auth.user)
 
-    return (
-        <StyledHeaderWrapper style={{ padding: '20px', backgroundColor: '#141414' }}>
-            <Flex alignItems='center' justifyContent='end'>
-                {(user.name && user.role) && (
-                    <>
-                        <Typography.Title level={4} style={{ marginTop: '0px' }}>ФИО: {user.name},</Typography.Title>
-                        <Typography.Title level={4} style={{ marginTop: '0px' }}>Роль: {roles[user.role]}</Typography.Title>
-                    </>
-                )}
-            </Flex>
-        </StyledHeaderWrapper>
-    )
+	return (
+		<StyledHeaderWrapper
+			style={{ padding: '20px', backgroundColor: '#141414' }}
+		>
+			<Flex
+				alignItems='center'
+				justifyContent='end'
+				style={{ padding: '0 20px' }}
+			>
+				{user.name && user.role && (
+					<>
+						<Typography.Title
+							level={4}
+							style={{ marginTop: '0px' }}
+						>
+							ФИО: {user.name},
+						</Typography.Title>
+						<Typography.Title
+							level={4}
+							style={{ marginTop: '0px' }}
+						>
+							Роль: {roles[user.role]}
+						</Typography.Title>
+					</>
+				)}
+			</Flex>
+		</StyledHeaderWrapper>
+	)
 }
 
 export default Header
